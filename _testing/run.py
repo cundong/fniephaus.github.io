ae14362443ad47d3a84a5662e17803b672df0af1
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-04-03 22:40:01 +0200

    Add w3c tests

---
+++ _testing/run.py
@@ -0,0 +1,17 @@
+import sys
+import time
+from py_w3c.validators.html.validator import HTMLValidator
+
+with open('CNAME', 'r') as f:
+    cname = f.readline()
+
+vld = HTMLValidator()
+vld.validate("http://%s/" % cname)
+
+if len(vld.errors) > 0:
+    for error in vld.errors:
+        print "#%s: %s" % (error['line'], error['message'])
+    sys.exit(1)
+
+print ''
+print 'W3C validation passed'
