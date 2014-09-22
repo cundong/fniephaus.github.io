32d0e281952c5520ef17a43d1f075a9d9f70e0b8
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-09-21 23:31:56 -0700

    Rewrite deep linking mechanism

---
+++ js/core.js
@@ -0,0 +1,41 @@
+$(function() {
+    var agent = navigator.userAgent,
+        is_ipad = agent.match(/iPad/i) !== null,
+        is_iphone = !is_ipad && ((agent.match(/iPhone/i) !== null) || (agent.match(/iPod/i) !== null)),
+        is_ios = is_ipad || is_iphone,
+        is_android = !is_ios && agent.match(/android/i) !== null,
+        is_mobile = is_ios || is_android,
+        new_window = false,
+        Browser_Hidden = function () {
+            if (typeof document.hidden !== 'undefined') {
+              return document.hidden;
+            } else if (typeof document.mozHidden !== 'undefined') {
+              return document.mozHidden;
+            } else if (typeof document.msHidden !== 'undefined') {
+              return document.msHidden;
+            } else if (typeof document.webkitHidden !== 'undefined') {
+              return document.webkitHidden;
+            }
+            return false;
+        };
+
+    $('a').click(function( event ) {
+        if (is_mobile) {
+            var href = $(this).attr('href');
+            var app_link = $(this).attr('data-app');
+            if (typeof app_link !== typeof undefined && app_link !== false) {
+                setTimeout(function () {
+                    if (new_window !== false && Browser_Hidden()){
+                        new_window.close();
+                    } else {
+                        new_window = window.open(href);
+                    }
+                }, 25);
+                window.location = app_link;
+            }
+            event.preventDefault();
+        }
+    });
+});
+
+
